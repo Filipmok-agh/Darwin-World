34fc38c4159489e3577b4ee7308b18b851340b8a
Filip <filipmok@student.agh.edu.pl>
2024-12-28 21:27:25 +0100

    idea

--- DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Maps/AbstractMap.java
+++ DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Maps/AbstractMap.java
@@ -6,18 +6,21 @@ import agh.ics.oop.darwinWorld.Elements.Vector2d;
 import agh.ics.oop.darwinWorld.Elements.WorldElement;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Random;
 
 import static agh.ics.oop.darwinWorld.Config.*;
 
 public abstract class AbstractMap implements WorldMap {
-    protected HashMap<Vector2d, LinkedList<Animal>> animals;
+    protected LinkedList<Animal> animals;
     protected HashMap<Vector2d, Grass> grasses;
+    protected LinkedHashMap<Vector2d, LinkedList<Animal>> dailyAnimals;
 
     public AbstractMap() {
-        this.animals = new HashMap<>();
+        this.animals = new LinkedList<>();
         this.grasses = new HashMap<>();
+        this.dailyAnimals = new LinkedHashMap<>();
     }
 
     public void addInitialAnimals(){
@@ -27,9 +30,53 @@ public abstract class AbstractMap implements WorldMap {
             int randomY = random.nextInt(mapHeight);
             Vector2d position = new Vector2d(randomX, randomY);
             Animal animal = new Animal(position);
-            addAnimal(animal);
+            //Dodajemy zwierzęta do linkedlisty zwierząt
+            this.animals.add(animal);
         }
     }
+    public void animalsMovement()
+    {
+        this.dailyAnimals = new LinkedHashMap<>();
+        for(Animal animal : animals)
+        {
+            animal.sleep();
+            if(animal.getEnergy()<=0)
+            {
+                //tutaj wiadomo usuwamy go ustawiamy date smierci itp
+            }
+            else
+            {
+                animal.move();
+                LinkedList<Animal> temp = new LinkedList<>();
+                temp = dailyAnimals.get(animal.getPosition());
+                if(temp!=null)
+                {
+                    addAnimal(temp, animal);
+                }
+                else
+                {
+                    LinkedList<Animal> newList = new LinkedList<>();
+                    newList.add(animal);
+                    dailyAnimals.put(animal.getPosition(), newList);
+                }
+            }
+        }
+    }
+
+    protected void addAnimal(LinkedList<Animal> animalsAtPosition, Animal animal) {
+        boolean inserted = false;
+        for (int i = 0; i < animalsAtPosition.size(); i++) {
+            if (animal.isStronger(animalsAtPosition.get(i))) {
+                animalsAtPosition.add(i, animal);
+                inserted = true;
+                break;
+            }
+        }
+        if (!inserted) {
+            animalsAtPosition.addLast(animal);
+        }
+    }
+
 
     public void place(WorldElement element) {
         if (element instanceof Animal) {
@@ -40,22 +87,6 @@ public abstract class AbstractMap implements WorldMap {
         }
     }
 
-    private void addAnimal(Animal animal) {
-        Vector2d position = animal.getPosition();
-        LinkedList<Animal> animalsAtPosition = animals.computeIfAbsent(position, k -> new LinkedList<>());
-
-        boolean inserted = false;
-        for (int i = 0; i < animalsAtPosition.size(); i++) {
-            if (((Animal) animal).isStronger(animalsAtPosition.get(i))) {
-                animalsAtPosition.add(i, animal);
-                inserted = true;
-                break;
-            }
-        }
-        if (!inserted) {
-            animalsAtPosition.addLast(animal);
-        }
-    }
 
     abstract public void spawnGrass();
 }
