aa0fb1b02d1384c3a2ec25d23c625b56c1019056
Filip <filipmok@student.agh.edu.pl>
2024-12-29 16:06:14 +0100

    method's name fix

--- DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Elements/Vector2d.java
+++ DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Elements/Vector2d.java
@@ -17,7 +17,7 @@ public class Vector2d
         return new Vector2d(newX, newY);
     }
 
-    public boolean IsYInRange(int bottom, int top) {
+    public boolean isYInRange(int bottom, int top) {
         return this.y >= bottom && this.y <= top;
     }
 
