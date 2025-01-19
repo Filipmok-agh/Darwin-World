154bbfb013d708dfe3143edd7af9af47003d67b6
Filip <filipmok@student.agh.edu.pl>
2025-01-19 14:16:50 +0100

    Setting Config

--- DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Elements/Animal.java
+++ DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/Elements/Animal.java
@@ -1,5 +1,6 @@
 package agh.ics.oop.darwinWorld.Elements;
 
+import agh.ics.oop.darwinWorld.Config;
 import agh.ics.oop.darwinWorld.Maps.MapDirection;
 import javafx.scene.paint.Color;
 
@@ -20,22 +21,25 @@ public class Animal implements WorldElement {
     private int eatenGrass;
     private int daysAlive;
     private int funeralDay;
+    private Config config;
 
-    public Animal(Vector2d position) {
+    public Animal(Vector2d position,Config config) {
         this.initializeAnimalStats();
         this.direction = MapDirection.values()[new Random().nextInt(MapDirection.values().length)];
         this.position = position;
+        this.config = config;
         this.genes = new Genes();
-        this.energy = initialAnimalEnergy;
+        this.energy = config.initialAnimalEnergy;
         this.parents = new Animal[]{};
     }
 
     public Animal(Animal parent1, Animal parent2) {
         this.initializeAnimalStats();
+        this.config= parent1.config;
         this.direction = MapDirection.values()[new Random().nextInt(MapDirection.values().length)];
         this.position = parent1.getPosition();
         this.genes = new Genes(parent1, parent2);
-        this.energy = 2 * parentEnergyCost;
+        this.energy = 2 * config.parentEnergyCost;
         this.parents = new Animal[]{parent1, parent2};
     }
 
@@ -78,19 +82,19 @@ public class Animal implements WorldElement {
     }
 
     private void overLeftBound(Vector2d positionToProcess) {
-        this.position = new Vector2d(mapWidth - 1, positionToProcess.getY());
+        this.position = new Vector2d(config.mapWidth - 1, positionToProcess.getY());
     }
 
 
     public void move() {
-        this.energy = this.energy - dailyEnergyCost;
+        this.energy = this.energy - config.dailyEnergyCost;
         this.daysAlive++;
         Integer currentDirection = this.getDirection().toNumber();
         Integer currentMove = this.genes.curr();
         MapDirection directionToProcess = MapDirection.fromNumber((currentDirection + currentMove) % 8);
         Vector2d positionToProcess = this.getPosition().add(directionToProcess.toUnitVector());
-        if (positionToProcess.isYInRange(0, mapHeight - 1)) {
-            if (positionToProcess.isXGreaterThan(mapWidth - 1)) {
+        if (positionToProcess.isYInRange(0, config.mapHeight - 1)) {
+            if (positionToProcess.isXGreaterThan(config.mapWidth - 1)) {
                 this.overRightBound(positionToProcess);
             } else if (positionToProcess.isXLessThan(0)) {
                 this.overLeftBound(positionToProcess);
@@ -106,7 +110,7 @@ public class Animal implements WorldElement {
 
     public void eat() {
         this.eatenGrass++;
-        this.energy = this.energy + plantEnergy;
+        this.energy = this.energy + config.plantEnergy;
     }
 
     private void updateDescendantsCount(HashSet<Animal> updatedDescendantsSet) {
@@ -126,8 +130,8 @@ public class Animal implements WorldElement {
         parent.children.add(child);
         this.childrenAmount = this.children.size();
         parent.childrenAmount = parent.children.size();
-        this.energy -= parentEnergyCost;
-        parent.energy -= parentEnergyCost;
+        this.energy -= config.parentEnergyCost;
+        parent.energy -= config.parentEnergyCost;
         return child;
     }
 
@@ -146,7 +150,7 @@ public class Animal implements WorldElement {
     }
 
     public Color getColor() {
-        double daysLeft = (double) energy / dailyEnergyCost;
+        double daysLeft = (double) energy / config.dailyEnergyCost;
         if (daysLeft > 30) {
             return Color.CYAN;
         }
