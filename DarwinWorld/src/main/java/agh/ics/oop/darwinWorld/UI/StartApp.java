921899038b063a6a15c19423b79625f9d0af86bb
Filip <filipmok@student.agh.edu.pl>
2025-01-22 14:10:43 +0100

    Create startApp as the landing page for the Darwin World application

---
+++ DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/UI/StartApp.java
@@ -0,0 +1,18 @@
+package agh.ics.oop.darwinWorld.UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Start.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Darwin World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ DarwinWorld/src/main/java/agh/ics/oop/darwinWorld/UI/StartPresenter.java
@@ -0,0 +1,28 @@
+package agh.ics.oop.darwinWorld.UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class StartPresenter
+{
+
+    public Button startButton;
+
+    @FXML
+    private void initialize()
+    {
+        startButton.setOnAction(event ->
+        {
+            MenuApp menuApp = new MenuApp();
+            try {
+                menuApp.start(new Stage());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            Stage currentStage = (Stage) startButton.getScene().getWindow();
+            currentStage.close();
+        });
+    }
+}
